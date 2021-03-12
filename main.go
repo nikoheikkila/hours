457d317ec09510ab28091a11ad2d8abec1655c72
Niko Heikkilä <niko.heikkila@futurice.com>
2021-03-12 15:22:16 +0200

    refactor: organize Go modules

--- main.go
+++ main.go
@@ -2,48 +2,15 @@ package main
 
 import (
 	"fmt"
-	"math"
 
-	termenv "github.com/muesli/termenv"
+	"github.com/muesli/termenv"
+	"github.com/nikoheikkila/toggl-reporter/report"
 )
 
-type Report struct {
-	hours float64
-	project string
-	description string
-}
-
-func NewReport(hours float64, project string, description string) Report {
-	return Report{
-		hours: math.Ceil(hours * 100) / 100,
-		project: project,
-		description: description,
-	}
-}
-
-func (r Report) Hours() termenv.Style {
-	out := fmt.Sprintf("%.1f", r.hours)
-	return termenv.String(out).Bold().Foreground(termenv.ANSIMagenta)
-}
-
-func (r Report) Project() termenv.Style {
-	return termenv.String(r.project).Bold().Foreground(termenv.ANSIBrightRed)
-}
-
-func (r Report) Description() termenv.Style {
-	return termenv.String(r.description).Bold().Foreground(termenv.ANSIYellow)
-}
-
-func (r Report) ToString() termenv.Style {
-	report := fmt.Sprintf("%s hours in %s with task '%s'", r.Hours(), r.Project(), r.Description())
-
-	return termenv.String(report).Foreground(termenv.ANSIBrightGreen)
-}
-
 func main() {
-	var reports []Report
+	var reports []report.Report
 
-	reports = append(reports, Report{1.20, "Test Project", "Meeting"})
+	reports = append(reports, report.New(1.2, "Test Project", "Meeting"))
 
 	for i, report := range reports {
 		index := termenv.String(fmt.Sprint(i + 1)).Bold().Foreground(termenv.ANSIBrightYellow)
