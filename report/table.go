62d0552c7ff2888b94e33418d853ff7f450be575
Niko Heikkilä <niko.heikkila@futurice.com>
2021-03-15 13:40:58 +0200

    feat(go): calculate total spent hours for table-based reports

--- report/table.go
+++ report/table.go
@@ -5,16 +5,25 @@ import (
 
 	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/nikoheikkila/hours/toggl"
+	formats "github.com/nikoheikkila/hours/toggl/utils"
 )
 
+var headerRow = table.Row{"Date", "Task", "Project", "Client", "Hours"}
+
 func PrepareTable(entries []toggl.TimeEntry) table.Writer {
+	var totalMilliSeconds int64
+
 	markdown := table.NewWriter()
 	markdown.SetOutputMirror(os.Stdout)
-	markdown.AppendHeader(table.Row{"Date", "Task", "Project", "Client", "Hours"})
+	markdown.AppendHeader(headerRow)
 
 	for _, entry := range entries {
+		totalMilliSeconds += entry.Duration
 		markdown.AppendRow(table.Row{entry.FormatStartDate(), entry.Description, entry.GetProject(), entry.GetClient(), entry.FormatHours()})
 	}
+
+	totalHours := formats.FormatDuration(formats.MillisecondsToHours(totalMilliSeconds))
+	markdown.AppendFooter(table.Row{"Total", "", "", "", totalHours})
 
 	return markdown
 }
--- toggl/utils/formats.go
+++ toggl/utils/formats.go
@@ -1,5 +1,11 @@
 package utils
 
+import "fmt"
+
 func MillisecondsToHours(seconds int64) float64 {
 	return float64(seconds) / 1000 / 60 / 60
+}
+
+func FormatDuration(duration float64) string {
+	return fmt.Sprintf("%.1f", duration)
 }
