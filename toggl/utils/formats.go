c484db251c48c7ee5b76291db4bebdce205dc36e
Niko Heikkilä <niko.heikkila@futurice.com>
2021-03-17 12:56:17 +0200

    refactor(go): use constants when converting milliseconds to hours

--- toggl/utils/formats.go
+++ toggl/utils/formats.go
@@ -2,8 +2,10 @@ package utils
 
 import "fmt"
 
+const MILLISECONDS_IN_HOURS float64 = 1000 * 60 * 60
+
 func MillisecondsToHours(seconds int64) float64 {
-	return float64(seconds) / 1000 / 60 / 60
+	return float64(seconds) / MILLISECONDS_IN_HOURS
 }
 
 func FormatDuration(duration float64) string {
